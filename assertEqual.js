33061464ab7e59ba7dce9c09188ea5a84c9af748
Andrew Roth <a.roth.80@hotmail.com>
2024-10-25 16:07:43 -0700

    Comparison and emojis added

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,14 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {};
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+      console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+      console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs"); // Assertion Passed
+assertEqual("Lighthouse Labs", "Bootcamp"); // Assertion Failed
+assertEqual(1, 1); // Assertion Passed
+assertEqual(1, 2); // Assertion Failed

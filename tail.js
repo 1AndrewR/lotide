8366dfa3ff8f5ce7e18307fda820b29b35563936
Andrew Roth <a.roth.80@hotmail.com>
2024-10-25 16:51:44 -0700

    tests added to tail.js

--- tail.js
+++ tail.js
@@ -9,3 +9,9 @@ const assertEqual = function(actual, expected) {
 const tail = function(array) {
   return array.slice(1);
 };
+
+// TEST CODE
+assertArraysEqual(tail([5, 6, 7]), [6, 7]);
+assertArraysEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+assertArraysEqual(tail([1]), []);
+assertArraysEqual(tail([]), []);

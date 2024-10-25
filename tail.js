e4d8874194b90e23e877c820e25dea9977e78da2
Andrew Roth <a.roth.80@hotmail.com>
2024-10-25 16:55:41 -0700

    tests updated in tail.js

--- tail.js
+++ tail.js
@@ -11,7 +11,11 @@ const tail = function(array) {
 };
 
 // TEST CODE
-assertArraysEqual(tail([5, 6, 7]), [6, 7]);
-assertArraysEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
-assertArraysEqual(tail([1]), []);
-assertArraysEqual(tail([]), []);
+const words = ["Hello", "Lighthouse", "Labs"];
+tail(words); 
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+assertEqual(tail(words)[0], "Lighthouse") // first element of returned array should be second element of argument array
+assertEqual(tail(words).length, words.length - 1) // tail should have one element less than its input
+assertEqual(tail([1]).length, 0) // tail of array with one element only should be empty
+assertEqual(tail([]).length, 0) // tail of an empty array should also be empty

fe53c758cae863acb7da09d686ea47ef0921b3e5
Andrew Roth <a.roth.80@hotmail.com>
2024-10-27 01:05:14 -0700

    eqArrays.js working

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+      console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+      console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function eqArrays(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Tests
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); 
+assertEqual(eqArrays([], []), true); 
+assertEqual(eqArrays([1, 2], [1, 2, 3]), false); 
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false); 

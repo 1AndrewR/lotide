69885e599d7abc57ed1edea77360627f0040f592
Andrew Roth <a.roth.80@hotmail.com>
2024-10-30 15:03:08 -0700

    countLetters.js working

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+      console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+      console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const results = {};
+
+  for (const letter of sentence) {
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+// test
+const result = countLetters("LHL");
+console.log(result);

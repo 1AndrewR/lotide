879aaed882508e7100e8c1dd5b11e263f6ed5fcd
Andrew Roth <a.roth.80@hotmail.com>
2024-11-01 14:22:51 -0700

    assertObjectsEqual.js working

---
+++ assertObjectsEqual.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  console.log("Keys1:", keys1);
+  console.log("Keys2:", keys2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect; 
+  
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};

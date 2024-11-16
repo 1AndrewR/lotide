ef241d2e80326f511f1a1744ac35d8df701d5141
Andrew Roth <a.roth.80@hotmail.com>
2024-11-16 11:47:43 -0800

    3rd test added to middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -8,4 +8,9 @@ describe("#middle", () => {
   it("should return [3] for [1, 2, 3, 4, 5]", () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
   });
+  it("make sure the original array was not altered by the middle function", () => {
+    const words = ["hello", "world", "lighthouse"];
+    const result = middle(words);
+    assert.strictEqual(words.length, 3);
+  });
 });

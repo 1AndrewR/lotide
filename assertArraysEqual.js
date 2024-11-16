86812f103a170709a4413b8464f6af2a6f2e6d06
Andrew Roth <a.roth.80@hotmail.com>
2024-11-16 10:40:08 -0800

    assertArraysEqual.js refactored with require and export

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-      console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-      console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
+const eqArrays = require("./eqArrays");
 
-function eqArrays(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log(`✅  Assertion passed: ${array1} === ${array2}`);
@@ -24,3 +8,4 @@ const assertArraysEqual = function(array1, array2) {
     console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
   }
 };
+module.export = assertArraysEqual;
